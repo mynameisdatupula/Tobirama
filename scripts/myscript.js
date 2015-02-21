8d4193d65b1126099203f437562d2be7a7868b5b
Datu Pula <danielorencio@gmail.com>
2015-02-21 09:29:00 +0800

    First commit

---
+++ scripts/myscript.js
@@ -0,0 +1,249 @@
+$(document).ready(function(){
+
+			if($(window).width() < 1100 && $(window).width() > 860 )
+			{
+				//$("#navId").html().html().css("font-size","10px");
+				$("#navId").children().children().css("font-size" , "20px");
+				$("#navId").children(".liHome").css({"padding-left": "20px" , "padding-right":"70px"});
+				$("#navId").children(".liAbout").css({"padding-left": "60px" , "padding-right":"70px"});
+				$("#navId").children(".liSkills").css({"padding-left": "60px" , "padding-right":"70px"});
+				$("#navId").children(".liDeath").css({"padding-left": "60px" , "padding-right":"70px"});
+				$("#navId").children(".liGallery").css({"padding-left": "60px" , "padding-right":"30px"});
+			//	alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+	
+
+				//document.alert($("#navId").html().html().css("font-size"));
+				
+			}
+
+			else if($(window).width() <= 860 && $(window).width() > 718 )
+			{
+				$("#navId").children().children().css("font-size" , "20px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"55px"});
+				$("#navId").children(".liAbout").css({"padding-left": "45px" , "padding-right":"55px"});
+				$("#navId").children(".liSkills").css({"padding-left": "45px" , "padding-right":"55px"});
+				$("#navId").children(".liDeath").css({"padding-left": "45px" , "padding-right":"55px"});
+				$("#navId").children(".liGallery").css({"padding-left": "45px" , "padding-right":"20px"});
+		//		alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 718 && $(window).width() > 585 )
+			{
+				$("#navId").children().children().css("font-size" , "15px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"40px"});
+				$("#navId").children(".liAbout").css({"padding-left": "40px" , "padding-right":"50px"});
+				$("#navId").children(".liSkills").css({"padding-left": "40px" , "padding-right":"50px"});
+				$("#navId").children(".liDeath").css({"padding-left": "40px" , "padding-right":"50px"});
+				$("#navId").children(".liGallery").css({"padding-left": "40px" , "padding-right":"20px"});
+			//	alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 585 && $(window).width() > 470 )
+			{
+				$("#navId").children().children().css("font-size" , "12px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"30px"});
+				$("#navId").children(".liAbout").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liSkills").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liDeath").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liGallery").css({"padding-left": "30px" , "padding-right":"20px"});
+			//	alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 470 && $(window).width() > 430 )
+			{
+				$("#navId").children().children().css("font-size" , "10px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"30px"});
+				$("#navId").children(".liAbout").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liSkills").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liDeath").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liGallery").css({"padding-left": "30px" , "padding-right":"20px"});
+				//alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+			else if($(window).width() <= 430  && $(window).width() > 397)
+			{
+				$("#navId").children().children().css("font-size" , "10px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"24px"});
+				$("#navId").children(".liAbout").css({"padding-left": "24px" , "padding-right":"34px"});
+				$("#navId").children(".liSkills").css({"padding-left": "24px" , "padding-right":"34px"});
+				$("#navId").children(".liDeath").css({"padding-left": "24px" , "padding-right":"34px"});
+				$("#navId").children(".liGallery").css({"padding-left": "24px" , "padding-right":"20px"});
+				alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 397 &&  $(window).width() > 364)
+			{
+				$("#navId").children().children().css("font-size" , "10px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"18px"});
+				$("#navId").children(".liAbout").css({"padding-left": "18px" , "padding-right":"28px"});
+				$("#navId").children(".liSkills").css({"padding-left": "18px" , "padding-right":"28px"});
+				$("#navId").children(".liDeath").css({"padding-left": "18px" , "padding-right":"28px"});
+				$("#navId").children(".liGallery").css({"padding-left": "18px" , "padding-right":"20px"});
+				alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 364)
+			{
+				$("#navId").children().children().css("font-size" , "9px");
+				$("#navId").children(".liHome").css({"padding-left": "9px" , "padding-right":"10px"});
+				$("#navId").children(".liAbout").css({"padding-left": "10px" , "padding-right":"20px"});
+				$("#navId").children(".liSkills").css({"padding-left": "10px" , "padding-right":"20px"});
+				$("#navId").children(".liDeath").css({"padding-left": "10px" , "padding-right":"20px"});
+				$("#navId").children(".liGallery").css({"padding-left": "10px" , "padding-right":"16px"});
+				alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+
+			else if($(window).width() >= 1100)
+			{
+				$("#navId").children().children().css("font-size" , "25px");
+				$("#navId").children(".liHome").css({"padding-left": "40px" , "padding-right":"90px"});
+				$("#navId").children(".liAbout").css({"padding-left": "80px" , "padding-right":"90px"});
+				$("#navId").children(".liSkills").css({"padding-left": "80px" , "padding-right":"90px"});
+				$("#navId").children(".liDeath").css({"padding-left": "80px" , "padding-right":"90px"});
+				$("#navId").children(".liGallery").css({"padding-left": "80px" , "padding-right":"50px"});
+			//	alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+
+			}
+
+
+		});
+
+		$(window).resize(function(){
+
+			if($(window).width() < 1100 && $(window).width() > 860 )
+			{
+				//$("#navId").html().html().css("font-size","10px");
+				$("#navId").children().children().css("font-size" , "20px");
+				$("#navId").children(".liHome").css({"padding-left": "20px" , "padding-right":"70px"});
+				$("#navId").children(".liAbout").css({"padding-left": "60px" , "padding-right":"70px"});
+				$("#navId").children(".liSkills").css({"padding-left": "60px" , "padding-right":"70px"});
+				$("#navId").children(".liDeath").css({"padding-left": "60px" , "padding-right":"70px"});
+				$("#navId").children(".liGallery").css({"padding-left": "60px" , "padding-right":"30px"});
+			//	alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+	
+
+				//document.alert($("#navId").html().html().css("font-size"));
+				
+			}
+
+			else if($(window).width() <= 860 && $(window).width() > 718 )
+			{
+				$("#navId").children().children().css("font-size" , "20px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"55px"});
+				$("#navId").children(".liAbout").css({"padding-left": "45px" , "padding-right":"55px"});
+				$("#navId").children(".liSkills").css({"padding-left": "45px" , "padding-right":"55px"});
+				$("#navId").children(".liDeath").css({"padding-left": "45px" , "padding-right":"55px"});
+				$("#navId").children(".liGallery").css({"padding-left": "45px" , "padding-right":"20px"});
+				//alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 718 && $(window).width() > 585 )
+			{
+				$("#navId").children().children().css("font-size" , "15px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"40px"});
+				$("#navId").children(".liAbout").css({"padding-left": "40px" , "padding-right":"50px"});
+				$("#navId").children(".liSkills").css({"padding-left": "40px" , "padding-right":"50px"});
+				$("#navId").children(".liDeath").css({"padding-left": "40px" , "padding-right":"50px"});
+				$("#navId").children(".liGallery").css({"padding-left": "40px" , "padding-right":"20px"});
+				//alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 585 && $(window).width() > 470 )
+			{
+				$("#navId").children().children().css("font-size" , "12px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"30px"});
+				$("#navId").children(".liAbout").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liSkills").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liDeath").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liGallery").css({"padding-left": "30px" , "padding-right":"20px"});
+				//alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+			else if($(window).width() <= 470 && $(window).width() > 430 )
+			{
+				$("#navId").children().children().css("font-size" , "10px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"30px"});
+				$("#navId").children(".liAbout").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liSkills").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liDeath").css({"padding-left": "30px" , "padding-right":"40px"});
+				$("#navId").children(".liGallery").css({"padding-left": "30px" , "padding-right":"20px"});
+				//alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+			else if($(window).width() <= 430  && $(window).width() > 397)
+			{
+				$("#navId").children().children().css("font-size" , "10px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"24px"});
+				$("#navId").children(".liAbout").css({"padding-left": "24px" , "padding-right":"34px"});
+				$("#navId").children(".liSkills").css({"padding-left": "24px" , "padding-right":"34px"});
+				$("#navId").children(".liDeath").css({"padding-left": "24px" , "padding-right":"34px"});
+				$("#navId").children(".liGallery").css({"padding-left": "24px" , "padding-right":"20px"});
+				alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 397 &&  $(window).width() > 364)
+			{
+				$("#navId").children().children().css("font-size" , "10px");
+				$("#navId").children(".liHome").css({"padding-left": "10px" , "padding-right":"18px"});
+				$("#navId").children(".liAbout").css({"padding-left": "18px" , "padding-right":"28px"});
+				$("#navId").children(".liSkills").css({"padding-left": "18px" , "padding-right":"28px"});
+				$("#navId").children(".liDeath").css({"padding-left": "18px" , "padding-right":"28px"});
+				$("#navId").children(".liGallery").css({"padding-left": "18px" , "padding-right":"20px"});
+				alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() <= 364)
+			{
+				$("#navId").children().children().css("font-size" , "9px");
+				$("#navId").children(".liHome").css({"padding-left": "9px" , "padding-right":"10px"});
+				$("#navId").children(".liAbout").css({"padding-left": "10px" , "padding-right":"20px"});
+				$("#navId").children(".liSkills").css({"padding-left": "10px" , "padding-right":"20px"});
+				$("#navId").children(".liDeath").css({"padding-left": "10px" , "padding-right":"20px"});
+				$("#navId").children(".liGallery").css({"padding-left": "10px" , "padding-right":"16px"});
+				alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+			}
+
+			else if($(window).width() >= 1100)
+			{
+				$("#navId").children().children().css("font-size" , "25px");
+				$("#navId").children(".liHome").css({"padding-left": "40px" , "padding-right":"90px"});
+				$("#navId").children(".liAbout").css({"padding-left": "80px" , "padding-right":"90px"});
+				$("#navId").children(".liSkills").css({"padding-left": "80px" , "padding-right":"90px"});
+				$("#navId").children(".liDeath").css({"padding-left": "80px" , "padding-right":"90px"});
+				$("#navId").children(".liGallery").css({"padding-left": "80px" , "padding-right":"50px"});
+				//alert($("#navId").children().children().css("font-size") + " " + $(window).width() );
+
+				
+
+			}
+
+
+		});
